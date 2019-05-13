c3ad23cede418b73c34a5423d5399d51767e69a8
Aleksandr Tarasov (exzanimo) <exzanimo@gmail.com>
2019-05-14 00:47:57 +0300

    Выполнено задание "17. UITabBar". Демонстрация возможностей перехода между VC с помощью иконок в нижней части экрана.

---
+++ UI_Course7 (UITabBar)/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  UI_Course7 (UITabBar)
+//
+//  Created by Александр Тарасов on 14/05/2019.
+//  Copyright © 2019 Aleksandr Tarasov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
